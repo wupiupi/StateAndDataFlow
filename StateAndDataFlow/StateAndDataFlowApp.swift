1da77b3c208e34d6dfd095351d389f8985119923
Paul <pasha.makey@gmail.com>
2024-03-09 11:39:19 +0300

    🔧 chore: Added environment objects / changed default Scene View

--- StateAndDataFlow/StateAndDataFlowApp.swift
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 @main
 struct StateAndDataFlowApp: App {
+    @State private var contentViewVM = ContentViewViewModel()
+    @StateObject private var loginViewVM = LoginViewViewModel()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootView()
         }
+        .environment(contentViewVM)
+        .environmentObject(loginViewVM)
     }
 }
