49cf5c346c409f6ea357a328fec59bdb11bd2e0a
Paul <pasha.makey@gmail.com>
2024-03-09 11:39:38 +0300

    🔧 chore: New directory

---
+++ StateAndDataFlow/Views/RootView.swift
@@ -0,0 +1,25 @@
+//
+//  RootView.swift
+//  StateAndDataFlow
+//
+//  Created by Paul Makey on 8.03.24.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    @EnvironmentObject private var loginViewVM: LoginViewViewModel
+    
+    var body: some View {
+        if loginViewVM.isLoggedIn {
+            ContentView()
+        } else {
+            LoginView()
+        }
+    }
+}
+
+#Preview {
+    RootView()
+        .environmentObject(LoginViewViewModel())
+}
